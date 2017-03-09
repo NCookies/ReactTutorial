2da8a17e1af1b4f249f7c213aa26e0da2f56a684
NCookies <swstar21c@gmail.com>
2017-03-09 17:18:53 +0900

    study about React grammer

--- components/tabs.js
+++ components/tabs.js
@@ -6,19 +6,41 @@ import {
 } from 'react-native';
 
 
-export default class Tabs extends Component {
+class TabButton extends Component {
   render() {
-    return(
-      <View style={styles.tabs}>
-
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{height: 30, width: 30, backgroundColor: 'gray'}}></View>
+        <Text style={{marginTop: 5, color: '#A0A0A0'}}>{this.props.name}</Text>
       </View>
     )
   }
 }
 
+export default class tabs extends Component {
+  render() {
+    return(
+      <View style={styles.taps}>
+        <TabButton name="홈"/>
+        <TabButton name="시간표"/>
+        <TabButton name="커뮤니티"/>
+        <TabButton name="모임"/>
+        <TabButton name="더보기"/>
+      </View>
+    )
+
+  }
+}
+
 const styles = StyleSheet.create({
-  tabs: {
-    height: 100,
-    backgroundColor: '#FFD54F',
+  taps: {
+    height: 60,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopWidth: 0.5,
+    borderColor: '#A0A0A0',
   },
+
 });
