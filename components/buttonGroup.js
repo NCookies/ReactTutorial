9262371af926da6e747bc96f7bd745e4b5b34335
NCookies <swstar21c@gmail.com>
2017-03-09 16:02:17 +0900

    make ButtonGroup with this.props

--- components/buttonGroup.js
+++ components/buttonGroup.js
@@ -6,11 +6,33 @@ import {
 } from 'react-native';
 
 
+class Button extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, height: 100, borderWidth: 0.5, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'gray'}} />
+        <Text>{this.props.name}</Text>
+      </View>
+    )
+  }
+}
+
 export default class ButtonGroup extends Component {
   render() {
     return(
       <View style={styles.buttonGroup}>
-
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <Button name="내 계정" />
+          <Button name="친구" />
+          <Button name="강의평가" />
+          <Button name="학점계산기" />
+        </View>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <Button name="쪽지함" />
+          <Button name="공지사항" />
+          <Button name="도움말" />
+          <Button name="설정" />
+        </View>
       </View>
     )
   }
